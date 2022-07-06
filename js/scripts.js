eaaf2c4ed36a532edb6d3f9226ef0d458ab844d8
ElenaKulikovaa <elena3227641@gmail.com>
2022-07-06 22:48:03 +0300

    Homework_1

---
+++ js/scripts.js
@@ -0,0 +1,85 @@
+let a1 = 5 % 3,
+    a2 = 3 % 5,
+    a3 = 5 + '3',
+    a4 = '5' - 3,
+    a5 = 75 + 'кг'
+    a6 = 785 * 653,
+    a7 = 100 / 25,
+    a8 = 0 * 0,
+    a9 = 0 / 2,
+    a10 = 89 / 0,
+    a11 = 98 + 2,
+    a12 = 5 - 98,
+    a13 = (8 + 56 * 4) / 5,
+    a14 = (9 - 12) * 7 / (5 + 2),
+    a15 = +"123",
+    a16 = 1 || 0,
+    a17 = false || true,
+    a18 = true > 0;
+
+    console.log(a1, typeof a1);
+    console.log(a2, typeof a2);
+    console.log(a3, typeof a3);
+    console.log(a4, typeof a4);
+    console.log(a5, typeof a5);
+    console.log(a6, typeof a6);
+    console.log(a7, typeof a7);
+    console.log(a8, typeof a8);
+    console.log(a9, typeof a9);
+    console.log(a10, typeof a10);
+    console.log(a11, typeof a11);
+    console.log(a12, typeof a12);
+    console.log(a13, typeof a13);
+    console.log(a14, typeof a14);
+    console.log(a15, typeof a15);
+    console.log(a16, typeof a16);
+    console.log(a17, typeof a17);
+    console.log(a18, typeof a18);
+
+
+let wPryam = 10,
+    hPryam = 23;
+    SPryam = wPryam * hPryam;
+
+    console.log(SPryam + ' см2', typeof SPryam);
+
+
+let dCilinda = a7,
+    hCilinda = 10,
+    Pi = 3.14,
+    VCilinda = Pi * dCilinda * hCilinda;
+
+    console.log(VCilinda + ' м3', typeof VCilinda);
+
+
+let rKrug = 5,
+    SKruga = Pi * rKrug ** 2;
+
+    console.log(SKruga + ' см2', typeof SKruga);
+
+
+let oTrap1 = 5,
+    oTrap2 = 7,
+    hTrap = 10,
+    STrap = (oTrap1 + oTrap2) / 2 * hTrap;
+
+    console.log(STrap + ' см2', typeof STrap);
+
+
+let SKredit = 2000000,
+    pKredit = 10,
+    yKredit = 5,
+    Pereplata = SKredit / pKredit * yKredit;
+
+    console.log(Pereplata + ' млн.руб.', typeof Pereplata);
+
+
+let a = 8,
+    b = 3,
+    x1 = (16 - a + 2 * b) / 2;
+    x2 = (a - 15 * b) / (b - 6);
+    x3 = 23780 / (1 + 2 + a + b);
+
+    console.log(x1, typeof x1);
+    console.log(x2, typeof x2);
+    console.log(x3, typeof x3);
